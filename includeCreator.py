a522b2f934ad5032f3f3e973b432ccf023853cf9
Glenn <hiddenillusion@gmail.com>
2018-04-10 15:07:47 -0400

    [-] includeCreator relevant again

--- includeCreator.py
+++ includeCreator.py
@@ -1,35 +1,60 @@
 # includeCreator.py was created by Glenn P. Edwards Jr.
-#	 	http://hiddenillusion.blogspot.com
-# 				@hiddenillusion
-# Version 0.0.1
-# Date: 12-04-2012
+#   https://hiddenillusion.github.io
+#        @hiddenillusion
+# Version 0.0.2
+# Date: 2012-12-04
 
 import os
-import argparse
+import sys
 
-def main():
+def RecursePath(path):
+  if not os.path.exists(path):
+    path = path.rstrip('"')
+    if not os.path.exists(path):
+      return
+
+  if os.path.exists(path):
+    if os.path.isdir(path):
+      for root, dirs, files in os.walk(path):
+        dirs.sort()
+        for name in sorted(files):
+          fname = os.path.join(root, name)
+          if os.path.isfile(fname):
+            yield fname
+          else:
+            pass
+
+if __name__ == "__main__":
+    import argparse
     parser = argparse.ArgumentParser(description='Creates an "include.yara" file which contains various other YARA signature files.')
-    parser.add_argument('-F', '--full', help='Include the full path to the other YARA signature files.', action='store_true')	
-    parser.add_argument('-o', '--out', help='Path to save the newly created "include.yara" file.', required=True)	
-    parser.add_argument('Path', help='Path to directory containing the YARA signature files.')
+    parser.add_argument('-f', '--full', help='Include the full path to the YARA signatue file(s).', action='store_true')
+    parser.add_argument('-d', '--dir', help='Include the dirname in the path to the YARA signature file(s)', action='store_true')
+    parser.add_argument('-o', '--out', help='Path to save the newly created "include.yara" file.', required=True)
+    parser.add_argument('Path', help='Path to the directory containing the YARA signature files.')
     args = vars(parser.parse_args())
 
     combined = 'include.yara'
-    dir = args['Path']
-    out_dir = args['out']	
-    new_file = os.path.join(os.path.abspath(out_dir), combined)	
+    use_full_path = args['full']
+    use_dirname = args['dir']
 
-    print "[+] Creating: %s" % new_file
-    with open(new_file, 'w') as outty:	
-        c = 0
-        for f in os.listdir(dir): # keeping it one level for right now
-            if f.endswith('yar') or f.endswith('yara') and f != os.path.basename(new_file):
-                print "[%s] Adding: %s" % (c,f)
-                if args['full']:
-                    f = os.path.abspath(f)			
-                outty.write('include "%s"\n' % f)
-                c += 1
-    #print "[+] (%s) files added..." % len(os.listdir(dir))	
-    
-if __name__ == "__main__":
-        main()
+    if use_full_path and use_dirname:
+        print("[!] Can only use --full or --dir")
+        sys.exit(1)
+
+    new_file = os.path.join(os.path.abspath(args['out']), combined)
+
+    print("[+] Creating: {0}".format(new_file))
+    with open(new_file, 'a') as outty:
+        cnt = 0
+        for filepath in RecursePath(args['Path']):
+            #if f.endswith('yar') or f.endswith('yara') and f != os.path.basename(new_file):
+            if use_full_path:
+                fname = os.path.abspath(filepath)
+            elif use_dirname:
+                fname = os.path.join(os.path.dirname(filepath), os.path.basename(filepath))
+            else:
+                fname = os.path.basename(filepath)
+
+            outty.write('include "{0}"\n'.format(fname))
+            cnt += 1
+    print("[-] ({0}) files added...".format(cnt))
